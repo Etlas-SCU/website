a094180a7790f4f8853f59f720197908f12bc3bd
AlaaaAhmed66 <alaaabdelreheem.66@gmail.com>
2023-03-09 04:17:17 +0200

    home-page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,14 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Button } from "@mui/material";
 import React, { useContext, useState } from "react";
 import styles from "./Home.module.css";
 import downLogo from "../../images/Icons/downLogo.png";
 import ContainedBtn from "../../components/containedBtn/ContainedBtn";
 import { NavLink } from "react-router-dom";
 import phone from "../../images/Pngs/Phone.png";
+import Statue2 from '../../images/Pngs/Statue2.png'
+import App from '../../images/Pngs/AppStore.png'
+import Play from '../../images/Pngs/PlayStore.png'
+import Search from '../../images/Icons/Search.png'
 // import { Context } from "../../components/Context/Context";
 
 export default function Home() {
@@ -13,8 +17,38 @@ export default function Home() {
 
   return (
     <Stack>
-      <Box className={styles.home__sec1}>{/* first section  */}</Box>
-
+      <Stack className={styles.home__sec1}>
+        <Stack direction='row' className={styles.home_row} >
+          <Box className={styles.home_col} >
+            <Box>
+              <p className={styles.home_title}>THE NEXT GENERATION OF TOURIST EXPLORATION IS HERE!</p>
+              <p className={styles.home_prag}>Etlas is designed to revolutionize the way tourists explore new places. The app makes use of computer vision and augmented reality technologies to enhance the tourist experience by providing interactive and immersive experiences at famous landmarks and monuments.</p>
+            </Box>
+            <Stack className={styles.search} direction='row'>
+              <input
+                type='search'
+                placeholder='Search for monuments, statues, landmarks and more!'
+                name='search'
+                className={styles.search_input} />
+              <img src={Search} className={styles.search_icon} alt='search' />
+            </Stack>
+            <Stack className={styles.download_prag} direction='row'>
+              For a better experience <span className={styles.download}> download </span> our App
+            </Stack>
+            <Stack className={styles.download_icons} direction='row'>
+              <Button className={styles.download_btn}>
+                <img src={Play} className={styles.playstore_img} alt='download-with-play-store' />
+              </Button>
+              <Button className={styles.download_btn}>
+                <img src={App} className={styles.appstore_img} alt='download-with-app-store' />
+              </Button>
+            </Stack>
+          </Box>
+          <Box className={styles.img_col}>
+            <img src={Statue2} className={styles.home_img} alt='etlas-home' />
+          </Box>
+        </Stack>
+      </Stack>
       <Stack className={styles.home__sec2}>
         <Box>
           <Box className={styles.home__sec2__RSide}>
@@ -50,6 +84,6 @@ export default function Home() {
           </Box>
         </Box>
       </Stack>
-    </Stack>
+    </Stack >
   );
 }
