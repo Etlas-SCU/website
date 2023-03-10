41e921f94eddc63eb967814444def042f6a97f03
sohila <sohilakaraly958@gmail.com>
2023-03-11 00:01:11 +0200

    add articles sec in home

---
+++ src/components/outLineBtn/OutLineBtn.js
@@ -0,0 +1,7 @@
+import { Box } from "@mui/system";
+import React from "react";
+import styles from "./OutLineBtn.module.css"
+
+export default function Sww() {
+  return <Box className={styles.btn}>See more!</Box>;
+}
