e7b3f97f69469d30987e69785a313a2f3a85e643
sohila <sohilakaraly958@gmail.com>
2023-03-26 18:05:35 +0200

    add transition to cards

--- src/components/ArticlesCard/ArticlesCard.js
+++ src/components/ArticlesCard/ArticlesCard.js
@@ -1,36 +1,35 @@
-import { Box} from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
 import article from "../../images/Pngs/article.png";
 import styles from "./ArticlesCard.module.css";
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import { Link } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 
-
-export default function ArticlesCard({id}) {
-
+export default function ArticlesCard({ id, className }) {
   return (
-    
-    <Box className={styles.card}>
-        <Box >
-        <img src={article} className={styles.card__img} alt="article"/>
+    <Box className={`${styles.cont} ${className}`}> 
+      <Fade left>
+        <Box className={styles.card}>
+          <Box className={styles.card__img}>
+            <img src={article} alt="article" />
+          </Box>
+          <Box width="90%" margin="0 auto">
+            <h2 className={styles.card__name}>Anubis</h2>
+            <p className={styles.card__about}>
+              Know more about
+              <br />
+              Anubis and his powers.
+            </p>
+            <p className={styles.card__date}>15 Jan 2023</p>
+            <Link to={`/articles/${id}`}>
+              <p className={styles.card__more}>
+                Learn more <KeyboardArrowRightIcon />
+              </p>
+            </Link>
+          </Box>
         </Box>
-       <Box ml="15px">
-       <h2 className={styles.card__name}>Anubis</h2>
-        <p className={styles.card__about}>
-          Know more about
-          <br />
-          Anubis and his powers.
-        </p>
-        <p className={styles.card__date}>15 Jan 2023</p>
-        
-        <Link to={`/articles/${id}`} style={{textDecoration:"none", color:"white"}}>
-
-        <p className={styles.card__more}>Learn more <KeyboardArrowRightIcon/></p>
-
-        </Link>
-
-       </Box>
-      </Box>
-    
+      </Fade>
+    </Box>
   );
 }
--- src/pages/Articles/Articles.js
+++ src/pages/Articles/Articles.js
@@ -2,19 +2,24 @@ import { Stack } from "@mui/material";
 import React from "react";
 import ArticlesCard from "../../components/ArticlesCard/ArticlesCard";
 import PageOutLine from "../../components/PageOutLine/PageOutLine";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 export default function Articles() {
   const hamada = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const { t } = useTranslation();
 
   return (
-    <PageOutLine value={t('Articles.title')}>
-      <Stack direction="row" flexWrap="wrap" justifyContent="center">
-        {hamada.map((ind) => (
-          <ArticlesCard id={ind} key={ind}/>
-        ))}
-      </Stack>
+    <PageOutLine value={t("Articles.title")}>
+        <Stack
+          direction="row"
+          flexWrap="wrap"
+          justifyContent="center"
+          mt="25px"
+        >
+          {hamada.map((ind) => (
+            <ArticlesCard id={ind} key={ind} />
+          ))}
+        </Stack>
     </PageOutLine>
   );
 }
