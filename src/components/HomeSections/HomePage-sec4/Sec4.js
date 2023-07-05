feb35185ce861cebd4cfdeccae52f2c31e7a480a
sohila <sohilakaraly958@gmail.com>
2023-07-05 18:10:18 +0300

    tours integration

--- src/components/HomeSections/HomePage-sec4/Sec4.js
+++ src/components/HomeSections/HomePage-sec4/Sec4.js
@@ -1,29 +1,40 @@
-import { Stack, Box } from '@mui/material';
-import React from 'react';
-import Style from './Sec4.module.css';
-import ToursCard from '../../ToursCard/ToursCard'
-import { NavLink } from 'react-router-dom';
-import OutLineBtn from '../../outLineBtn/OutLineBtn';
+import { Stack, Box } from "@mui/material";
+import React from "react";
+import Style from "./Sec4.module.css";
+import ToursCard from "../../ToursCard/ToursCard";
+import { NavLink } from "react-router-dom";
+import OutLineBtn from "../../outLineBtn/OutLineBtn";
+import { Context } from "../../Context/Context";
+import { useContext } from "react";
 
 export default function Sec4(props) {
-    let tour = [1, 2, 3, 4, 5, 6];
-    return (
-        <Stack className={Style.sec4}>
-            <Box className={Style.title}>
-                <p>Check popular tours</p>
-            </Box>
-            <Stack className={Style.tours_cards}>
-                <Box className={Style.tours}>
-                    {tour.map((value) => {
-                        return <ToursCard className={Style.cards} key={value} />
-                    })}
-                </Box>
-            </Stack>
-            <Box>
-                <NavLink to='/tours' >
-                    <OutLineBtn className={Style.see_more} value={("See more!")} />
-                </NavLink>
-            </Box>
-        </Stack>
-    );
+  const { Tours } = useContext(Context);
+  return (
+    <Stack className={Style.sec4}>
+      <Box className={Style.title}>
+        <p>Check popular tours</p>
+      </Box>
+      <Stack className={Style.tours_cards}>
+        <Box className={Style.tours}>
+          {Tours !== null ? (
+            Tours.map((tour) => (
+              <ToursCard
+                key={tour.id}
+                id={tour.id}
+                tour={tour}
+                className={Style.cards}
+              />
+            ))
+          ) : (
+            <div>no tours</div>
+          )}
+        </Box>
+      </Stack>
+      <Box>
+        <NavLink to="/tours">
+          <OutLineBtn className={Style.see_more} value={"See more!"} />
+        </NavLink>
+      </Box>
+    </Stack>
+  );
 }
