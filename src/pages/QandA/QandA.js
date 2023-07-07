e9bf13fb12340c1e9ef98bdb0a7ab24850f9283c
Waelahmed99 <waelahmed6599@gmail.com>
2023-07-07 23:25:05 +0300

    Added hint shuffle

--- src/pages/QandA/QandA.js
+++ src/pages/QandA/QandA.js
@@ -96,8 +96,26 @@ export default function QandA() {
   const handleHint = () => {
     var wrong = choices.filter((answer) => !isCorrectAnswer(answer));
     var right = choices.filter((answer) => isCorrectAnswer(answer));
-    setChoices([right[0], wrong[0]])
+    setChoices(shuffle([right[0], wrong[0]]))
   };
+
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
 
   if (isLoading) {
     return <h2 className={style.error}>Loading..</h2>;
